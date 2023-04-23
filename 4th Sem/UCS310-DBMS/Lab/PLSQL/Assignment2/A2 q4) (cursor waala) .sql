80fe303bcf7862eecb94b2c6aff9a79ffb0f6727
erHardikVerma <128423270+erHardikVerma@users.noreply.github.com>
2023-04-23 17:02:23 +0530

    Create A2 q4) (cursor waala) .sql

---
+++ 4th Sem/UCS310-DBMS/Lab/PLSQL/Assignment2/A2 q4) (cursor waala) .sql
@@ -0,0 +1,14 @@
+declare 
+my_empno datatype;
+my_ename datatype;
+my_total datatype;
+my_emp1 emp%rowtype
+cursor c1 is select empno,ename,total from emp1 where total=40;
+cursor c2 return my my_emp1
+
+begin
+open c1;
+fetch c1 into my_emp
+
+
+lab assignment 2 q4
